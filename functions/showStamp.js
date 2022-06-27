578ae03b5e9d6705676510a85604aad616d55d34
Yataro Fujinaga <yatarofujinaga@gmail.com>
2022-06-27 16:13:21 +0900

    FIrebaseFunctionnの設定

--- functions/showStamp.js
+++ functions/showStamp.js
@@ -1,28 +1,32 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
-const db = admin.database()
+const database = admin.firestore()
 
-const CONFIG = functions.config()
-const appDomain = CONFIG.app.domain
-const OGP_IMG_WIDTH = 1200
-const OGP_IMG_HEIGHT = 630
+const appDomain = process.env.APP_DOMAIN
+const appName = process.env.APP_NAME
 
 const func = functions.https.onRequest((req, res) => {
   const [, , stampId] = req.path.split('/')
-  return db
-    .ref('/stamps')
-    .doc(stampId)
+
+  return database
+    .collection('stamps')
+    .where('id', '==', stampId)
+    .limit(1)
     .get()
-    .then((snap) => {
-      if (!snap) {
-        res.status(404).end('404 Not Found')
-        return
+    .then((snapshot) => {
+      if (!snapshot.empty) {
+        const doc = snapshot.docs[0]
+        const stampObj = doc.data()
+        const html = createHtml(
+          stampObj.id,
+          stampObj.image_url,
+          stampObj.description
+        )
+        res.set('Cache-Control', 'public, max-age=600, s-maxage=600')
+        res.status(200).end(html)
+      } else {
+        // デフォルトのHTMLを返す
       }
-      const stockItem = snap ? snap.data() : {}
-      const uname = stockItem.uname || ''
-      const html = createHtml(uname, stampId)
-      res.set('Cache-Control', 'public, max-age=600, s-maxage=600')
-      res.status(200).end(html)
     })
     .catch((err) => {
       console.warn(err)
@@ -30,37 +34,37 @@ const func = functions.https.onRequest((req, res) => {
     })
 })
 
-const createHtml = (uname, stockid) => {
-  const SITEURL = `https://${appDomain}`
-  const PAGEURL = `${SITEURL}/stock/${stockid}`
-  const TITLE = `view ${escapeHtml(uname)}'s colorsets on colorinco`
-  const DESCRIPTION =
-    'カラーインコはTwitterでお気に入りしている画像のカラーパレットを表示・ストックできるサービスです。'
-  return `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>colorinco</title>
-    <meta property="og:title" content="${TITLE}">
-    <meta property="og:image" content="${SITEURL}/ogp/stockimg/${stockid}">
-    <meta property="og:image:width" content="${OGP_IMG_WIDTH}">
-    <meta property="og:image:height" content="${OGP_IMG_HEIGHT}">
-    <meta property="og:description" content="${DESCRIPTION}">
-    <meta property="og:url" content="${PAGEURL}">
-    <meta property="og:type" content="article">
-    <meta property="og:site_name" content="colorinco*カラーインコ">
-    <meta name="twitter:site" content="${SITEURL}">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="${TITLE}">
-    <meta name="twitter:image" content="${SITEURL}/ogp/stockimg/${stockid}">
-    <meta name="twitter:description" content="${DESCRIPTION}">
-  </head>
-  <body>
-    <script type="text/javascript">window.location="/_stock/${stockid}";</script>
-  </body>
-</html>
-`
+const createHtml = (stampId, imageUrl, description) => {
+  const siteUrl = `https://${appDomain}`
+  const pageUrl = `${siteUrl}/stamps/${stampId}`
+  const title = `${stampId}`
+  const siteName = appName
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>${title}</title>
+        <meta property="og:title" content="${title}">
+        <meta property="og:image" content="${imageUrl}">
+        <meta property="og:image:width" content="${process.env.OGP_IMG_WIDTH}">
+        <meta property="og:image:height" content="${process.env.OGP_IMG_HEIGHT}">
+        <meta property="og:description" content="${description}">
+        <meta property="og:url" content="${pageUrl}">
+        <meta property="og:type" content="article">
+        <meta property="og:site_name" content="${siteName}">
+        <meta name="twitter:site" content="${siteUrl}">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="${title}">
+        <meta name="twitter:image" content="${imageUrl}">
+        <meta name="twitter:description" content="${description}">
+      </head>
+      <body>
+        <script type="text/javascript">window.location="/stamps/${stampId}";</script>
+      </body>
+    </html>
+    `
 }
 
 module.exports = func
